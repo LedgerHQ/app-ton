fbe723f76448cb9b98e9c314109d3d639168bd67
GroM <jerome.caporossi@ledger.fr>
2025-05-22 11:13:38 +0200

    Bump version and fix CI

--- src/common/bip32_check.c
+++ src/common/bip32_check.c
@@ -4,12 +4,10 @@
 
 #include "bip32_check.h"
 
-bool check_bip32_path(uint8_t bip32_path_len, const uint32_t bip32_path[MAX_BIP32_PATH]) {
-    if (bip32_path_len <= 2) return false;
-
-    return bip32_path[0] == 0x8000002c && bip32_path[1] == 0x8000025f;
+bool check_bip32_path_len(uint8_t bip32_path_len) {
+    return ((bip32_path_len <= 2) ? false : true); 
 }
 
 bool check_global_bip32_path() {
-    return check_bip32_path(G_context.bip32_path_len, G_context.bip32_path);
+    return check_bip32_path_len(G_context.bip32_path_len);
 }
--- src/common/bip32_check.h
+++ src/common/bip32_check.h
@@ -5,20 +5,19 @@
 #include "../types.h"  // MAX_BIP32_PATH
 
 /**
- * Check if the given BIP32 path length and path array are valid.
+ * Check if the given BIP32 path length is valid.
  *
  * @param bip32_path_len The length of the BIP32 path.
- * @param bip32_path The array containing the BIP32 path.
  *
- * @return true if the BIP32 path is valid, false otherwise.
+ * @return true if the BIP32 path len is valid, false otherwise.
  *
  */
-bool check_bip32_path(uint8_t bip32_path_len, const uint32_t bip32_path[MAX_BIP32_PATH]);
+bool check_bip32_path_len(uint8_t bip32_path_len);
 
 /**
- * Check the bip32 path stored in G_context.
+ * Check the bip32 path len stored in G_context.
  *
- * @return true if the BIP32 path is valid, false otherwise.
+ * @return true if the BIP32 path len is valid, false otherwise.
  *
  */
 bool check_global_bip32_path();
