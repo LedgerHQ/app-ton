73ba1f2a4578ff3a7107228120599b04d999c0f9
krigga <krigga7@gmail.com>
2025-09-16 20:56:30 +0300

    chore: tests

--- tests/test_sign_cmd.py
+++ tests/test_sign_cmd.py
@@ -443,3 +443,81 @@ def test_sign_tx_clear_jetton(firmware, backend, navigator, test_name):
         sig, hash_b = unpack_sign_tx_response(response)
         assert hash_b == tx.transfer_cell().bytes_hash()
         assert check_signature_validity(pubkey, sig, hash_b)
+
+
+def test_sign_tx_jetton_high_query_id(firmware, backend, navigator, test_name):
+    import os
+
+    # Use the app interface instead of raw interface
+    client = BoilerplateCommandSender(backend)
+    # The path used for this entire test
+    path: str = "m/44'/607'/0'/0'/0'/0'"
+
+    # First we need to get the public key of the device in order to build the transaction
+    pubkey = client.get_public_key(path=path).data
+
+    # Enable blind signing and expert mode
+    if firmware.device.startswith("nano"):
+        navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH,
+                                        test_name + "/pretest",
+                                        [
+                                            NavInsID.RIGHT_CLICK,
+                                            NavInsID.BOTH_CLICK,
+                                            NavInsID.BOTH_CLICK,
+                                            NavInsID.RIGHT_CLICK,
+                                            NavInsID.BOTH_CLICK,
+                                            NavInsID.RIGHT_CLICK,
+                                            NavInsID.BOTH_CLICK,
+                                            NavInsID.RIGHT_CLICK,
+                                            NavInsID.BOTH_CLICK,
+                                            NavInsID.RIGHT_CLICK,
+                                            NavInsID.BOTH_CLICK,
+                                        ],
+                                        screen_change_before_first_instruction=False)
+    else:
+        navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH,
+                                        test_name + "/pretest",
+                                        [
+                                            NavInsID.USE_CASE_HOME_INFO,
+                                            NavIns(NavInsID.TOUCH, (354, 125)),
+                                            NavIns(NavInsID.TOUCH, (354, 272)),
+                                            NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT,
+                                        ],
+                                        screen_change_before_first_instruction=False)
+
+    # the actual address does not really matter
+    usdt_wallet_address = "EQD0sKn8DbS12U015TWOSpYmyJYYDC_7sxg1upaMxnBvTiX8"
+
+    # Create the transaction that will be sent to the device for signing
+    tx = Transaction(Address(usdt_wallet_address), SendMode.PAY_GAS_SEPARATLY, 0, 1686176000, True, 100000000,
+                        payload=JettonTransferPayload(100, Address("0:" + "0" * 64), forward_amount=1, query_id=((1 << 64) - 1)))
+    tx_bytes = tx.to_request_bytes()
+
+    # Send the sign device instruction.
+    # As it requires on-screen validation, the function is asynchronous.
+    # It will yield the result when the navigation is done
+    with client.sign_tx(path=path, transaction=tx_bytes):
+        # Validate the on-screen request by performing the navigation appropriate for this device
+        if firmware.device.startswith("nano"):
+            navigator.navigate_until_text_and_compare(NavInsID.RIGHT_CLICK,
+                                                        [NavInsID.BOTH_CLICK],
+                                                        "Approve",
+                                                        ROOT_SCREENSHOT_PATH,
+                                                        test_name + "/part0")
+        else:
+            navigator.navigate([
+                                    NavInsID.SWIPE_CENTER_TO_LEFT,
+                                ])
+            navigator.navigate_until_text_and_compare(NavInsID.USE_CASE_VIEW_DETAILS_NEXT,
+                                                        [NavInsID.USE_CASE_REVIEW_CONFIRM,
+                                                        NavInsID.USE_CASE_STATUS_DISMISS],
+                                                        "Hold to sign",
+                                                        ROOT_SCREENSHOT_PATH,
+                                                        test_name + "/part0",
+                                                        screen_change_before_first_instruction=False)
+
+    # The device as yielded the result, parse it and ensure that the signature is correct
+    response = client.get_async_response().data
+    sig, hash_b = unpack_sign_tx_response(response)
+    assert hash_b == tx.transfer_cell().bytes_hash()
+    assert check_signature_validity(pubkey, sig, hash_b)
